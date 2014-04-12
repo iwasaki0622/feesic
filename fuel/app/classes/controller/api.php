ce16cc3dd4dcbaa61787513b55e8c268b7de24c3
Toshihiko Ryugou <ryugou@geeks.ne.jp>
2014-04-12 21:57:56 +0900

    マートン・福留

--- fuel/app/classes/controller/api.php
+++ fuel/app/classes/controller/api.php
@@ -16,34 +16,49 @@ class Controller_Api extends \Fuel\Core\Controller_Rest
     public function post_index()
     {
 
+        $uid = "fu-ji-na-mi";
+
         //動き
-        // $motion = Input::post('motion');
-        $motion = 100;
+        $motion = Input::post('motion');
+
         //音声
-        // $sound = Input::post('sound');
-        $sound = 100;
+        $sound = Input::post('sound');
+
         //緯度
-        // $lat = Input::post('lat');
-        $lat = 30;
+        $lat = Input::post('lat');
+
         //経度
-        // $lon = Input::post('lon');
-        $lon = 30;
+        $lon = Input::post('lon');
 
 
         $feelingClass = Gomez_Feeling::getInstance();
 
-        $feeling = $feelingClass->analyzeFeeling($motion, $sound);
+        $feelingTypeId = $feelingClass->kuji($motion, $sound);
 
         $data = array(
-            'uid' => 'hoge',
+            'uid' => $uid,
             'motion' => $motion,
             'sound' => $sound,
             'lat' => $lat,
             'lon' => $lon,
         );
         $action_log = Model_ActionLog::forge($data);
-       $action_log->save();
+        $action_log->save();
 
+        $gracenoteJson = Gomez_Feeling::murton($feelingTypeId);
+        $songName = $gracenoteJson['hogehogeh'];
+
+        $youtubeJson = Gomez_Feeling::fukudome($songName);
+
+        $data = array(
+            'uid' => $uid,
+            'gracenote_json' => $gracenoteJson,
+            'feeling_type_id' => $feelingTypeId,
+            'youtube_json' => $youtubeJson,
+        );
+
+        $feeling = Model_Feeling::forge($data);
+        $feeling->save();
 
         header("Access-Control-Allow-Origin: *");
         $this->response($action_log);
--- fuel/packages/gomez/classes/feeling.php
+++ fuel/packages/gomez/classes/feeling.php
@@ -16,33 +16,56 @@ class Gomez_Feeling {
 		return self::$instance;
 	}
 	
-	/*
-	* 動きと音声によって感情を分析する
+	/**
+	* 久慈の様な動きと音声によって感情を分析する
 	*/
-	function analyzeFeeling($motion, $sound) {
+	function kuji($motion, $sound) {
 
 		$feeling = Model_FeelingType::find_all();
 
 		if($motion <= 50 && $sound > 50) {
 
-			return $feeling[0];	//喜
+			return $feeling[0]['feeling_type_id'];	//喜
 
 		}
 		else if($motion > 50 && $sound > 50) {
 
-			return $feeling[1];	//怒
+			return $feeling[1]['feeling_type_id'];;	//怒
 
 		}
 		else if($motion <= 50 && $sound <= 50) {
 
-			return $feeling[2];	//哀
+			return $feeling[2]['feeling_type_id'];;	//哀
 
 		}
 		else if($motion > 50 && $sound <= 50) {
 
-			return $feeling[3];	//楽
+			return $feeling[3]['feeling_type_id'];;	//楽
 
 		}
 
 	}
+
+
+    /**
+     * マートン級の活躍でグレースノートを検索する
+     * @param $felingTypeId
+     * @return string
+     */
+    static function murton($felingTypeId) {
+        $gracenoteJson = "";
+
+        return $gracenoteJson;
+    }
+
+    /**
+     * 福留程度の機能
+     * @param $songName
+     * @return string
+     */
+    static function fukudome($songName) {
+        $youtubeJson = "";
+
+        return $youtubeJson;
+    }
 }
