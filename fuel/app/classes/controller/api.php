31cb2ab59d94b5507ef3dbc584187566fe9d86d7
Toshihiko Ryugou <ryugou@geeks.ne.jp>
2014-04-12 19:00:37 +0900

    APIひな形実装

--- fuel/app/classes/controller/api.php
+++ fuel/app/classes/controller/api.php
@@ -4,9 +4,10 @@ class Controller_Api extends \Fuel\Core\Controller_Rest
 {
     public function get_index()
     {
+        $feeling = Model_Feeling::find_by("uid", "hoge");
 
         header("Access-Control-Allow-Origin: *");
-        $this->response(array());
+        $this->response($feeling);
     }
 
     public function post_index()
--- fuel/app/config/production/config.php
+++ fuel/app/config/production/config.php
@@ -5,4 +5,5 @@
 
 return array(
     'grocenote_api' => 'https://c9102592.web.cddbp.net/webapi/json/1.0/',
+    'youtube_developer_key' => 'AI39si7TIoupISrpk0i4Nwwb5VDxSh7hRgZMXHnH634Gqukf26Dp4UygFNMaHS6wPK03OEuY37GS1hq4N44gtHhevUPNanbp5w',
 );
