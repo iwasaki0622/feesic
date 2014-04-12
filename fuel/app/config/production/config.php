239b6ef6a323850ba4eee7379085de7f052e6795
iwasaki0622 <sci.iwasaki@gmail.com>
2014-04-12 21:52:09 +0900

    本番環境にもgracenoteのidを定義

--- fuel/app/config/production/config.php
+++ fuel/app/config/production/config.php
@@ -4,10 +4,8 @@
  */
 
 return array(
-<<<<<<< HEAD
-    'grocenote_api' => 'https://c9102592.web.cddbp.net/webapi/json/1.0/',
     'youtube_developer_key' => 'AI39si7TIoupISrpk0i4Nwwb5VDxSh7hRgZMXHnH634Gqukf26Dp4UygFNMaHS6wPK03OEuY37GS1hq4N44gtHhevUPNanbp5w',
-=======
     'gracenote_api' => 'https://c9102592.web.cddbp.net/webapi/json/1.0/',
->>>>>>> 8ed0596bebf2769386802f22cc53035e8891eb3d
+    'gracenote_userid' => '262144776716759932-FE0B1FFC2D096871BEE48F54C7AD0D1F',
+    'gracenote_client_id' => '9102592-9208790F774CCC51F890791B55AA9377',
 );
